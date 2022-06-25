49a6df563a2d929c408b5ef3177589cecc8b80d2
Sabato Francesco Longobardi <s.f.longobardi@gmail.com>
2022-06-25 09:50:52 +0200

    Initial Commit

---
+++ voiceSynth/voiceSynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceSynthApp.swift
+//  voiceSynth
+//
+//  Created by Sabato Francesco Longobardi on 25/06/22.
+//
+
+import SwiftUI
+
+@main
+struct voiceSynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
